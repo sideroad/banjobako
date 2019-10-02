ced165ae17daf81fa598cd216cf22e976d1b5dca
sideroad <sideroad.jp@gmail.com>
2019-10-02 22:57:43 +0900

    Fixed bug of file name

--- packages/vscode-extension-install/index.ts
+++ packages/vscode-extension-install/index.ts
@@ -3,8 +3,8 @@ import path from 'path';
 import { readFileSync, existsSync } from 'fs';
 
 const install = () => {
-  const jsonPath = path.join(process.cwd(), '.vscode/extentions.json');
-  if (existsSync) {
+  const jsonPath = path.join(process.cwd(), '.vscode/extensions.json');
+  if (existsSync(jsonPath)) {
     const jsonFile = readFileSync(jsonPath, 'utf-8');
     const json = JSON.parse(jsonFile);
     (json.recommendations || []).forEach((lib: string) => {

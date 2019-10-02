e7ebe80dac8e4e457d4984422be4de9365134219
sideroad <sideroad.jp@gmail.com>
2019-10-02 22:06:02 +0900

    Fixed package json

--- packages/vscode-recommendation-install/index.ts
+++ packages/vscode-recommendation-install/index.ts
@@ -1,14 +1,16 @@
 import { spawnSync } from 'child_process';
 import path from 'path';
-import { readFileSync } from 'fs';
+import { readFileSync, existsSync } from 'fs';
 
 const install = () => {
   const jsonPath = path.join(process.cwd(), '.vscode/extentions.json');
-  const jsonFile = readFileSync(jsonPath, 'utf-8');
-  const json = JSON.parse(jsonFile);
-  json.recommendations.forEach((lib: string) => {
-    spawnSync('code', ['--install-extension', lib, '--force']);
-  });
+  if (existsSync) {
+    const jsonFile = readFileSync(jsonPath, 'utf-8');
+    const json = JSON.parse(jsonFile);
+    (json.recommendations || []).forEach((lib: string) => {
+      spawnSync('code', ['--install-extension', lib, '--force']);
+    });
+  }
 };
 
 install();

052395a1d130afa280dece913c7a9c7537551b5e
sideroad <sideroad.jp@gmail.com>
2019-11-08 19:22:16 +0900

    Change babel setting for edge

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,10 @@ module.exports = {
     '@babel/preset-react',
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript'
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-runtime'
   ]
 };

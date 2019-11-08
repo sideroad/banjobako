48e9f7cee8ede4145e90a0b0041cc91353dd95ea
sideroad <sideroad.jp@gmail.com>
2019-11-08 19:26:36 +0900

    Add licenses

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,6 @@ module.exports = {
   ],
   plugins: [
     '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-transform-runtime'
+    '@babel/plugin-proposal-object-rest-spread'
   ]
 };
